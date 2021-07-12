81acbbe336d3e23e87b90630757360b594191db6
andrearep <andrea.repe@gmail.com>
2021-07-12 17:40:18 +0200

    aggiunti dati stampati e stile

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -21,6 +21,7 @@
     body {
         width: 1200px;
         margin: auto;
+        background-color: #303030;
     }
 
     .cards {
@@ -38,6 +39,12 @@
         padding: 2rem;
         background-color: greenyellow;
         min-height: 150px;
+        border-radius: 5px;
+    }
+
+    h2 {
+        text-align: center;
+        margin-bottom: 1.5rem;
     }
     </style>
 </head>
@@ -46,9 +53,12 @@
     <div class="cards">
         @foreach($movies AS $movie)
         <div class="card">
-            <h4>Title: {{$movie['title']}}</h4>
-            <span>Original Title: {{$movie['original_title']}}</span>
-            <span>Nazionalità: {{$movie['nationality']}}</span>
+            <h2>{{$movie['title']}}</h2>
+            <span> <strong>Original Title: </strong> {{$movie['original_title']}}</span>
+            <span> <strong> Nazionalità: </strong> {{$movie['nationality']}}</span>
+            <span> <strong> Date: </strong> {{$movie['date']}}</span>
+            <span> <strong> Vote: </strong> {{$movie['vote']}}</span>
+
         </div>
         @endforeach
 
